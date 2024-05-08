32b7bc30f077b140143c88ee4e7655cfd5240bcd
Farhaan <59924029+0xfarhaan@users.noreply.github.com>
2024-05-08 20:44:29 +0400

    feat: address audit issue 3 (#10)

--- contracts/utils/SyrupRateProvider.sol
+++ contracts/utils/SyrupRateProvider.sol
@@ -7,14 +7,16 @@ import { IPoolLike } from "../interfaces/Interfaces.sol";
 
 contract SyrupRateProvider is ISyrupRateProvider {
 
-    address immutable public override pool;
+    uint256 public constant FIXED_POINT_ONE = 1e18;  // 18 decimal fixed point number.
+
+    address public immutable override pool;
 
     constructor(address pool_) {
         pool = pool_;
     }
 
     function getRate() external view override returns (uint256) {
-        return IPoolLike(pool).convertToExitAssets(1e18);
+        return IPoolLike(pool).convertToExitAssets(FIXED_POINT_ONE);
     }
 
 }
